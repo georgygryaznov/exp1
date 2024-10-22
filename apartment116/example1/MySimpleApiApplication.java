744cdd707926c936048cab5e376cfe4f151b57f8
Грязнов Георгий Сергеевич <GGryaznov@inno.local>
2024-10-22 15:57:00 +0500

    Initial commit

---
+++ apartment116/example1/MySimpleApiApplication.java
@@ -0,0 +1,12 @@
+package apartment116.example1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySimpleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySimpleApiApplication.class, args);
+    }
+}
---
+++ apartment116/example1/MySimpleApiApplication2.java
@@ -0,0 +1,12 @@
+package apartment116.example1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySimpleApiApplication2 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySimpleApiApplication2.class, args);
+    }
+}
